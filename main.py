42eecdde04c8e259bb2c754c12e7fe2fad93432c
Nacho <nachonava98@gmail.com>
2022-06-07 17:58:40 +0200

    Main entry point changes and tf-idf + bigrams for topics

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ logger = logging_factory.get_module_logger("main", logging.DEBUG)
 
 
 def main(argv):
-    subreddit_control = argv[0]
-    preprocess_inputs(subreddit_control)
-    extract_topics()
+    option = argv[0]
+    if option == "-pre":
+        subreddit_control = argv[1]
+        if subreddit_control is None or subreddit_control == "":
+             logger_err.error("Arguments expected as: -pre <subreddit_control>")
+        else:
+            preprocess_inputs()
+        preprocess_inputs(subreddit_control)
+    elif option == "-top":
+        extract_topics()
+    else:
+        logger_err.error("No options provided in the arguments: \n\t* '-pre' (to generate .txt files given submission .jsonl files) \n\t* '-top' (to generate the .html files with all the topics given the .txt files)")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
         main(sys.argv[1:])
-    else:
-        logger_err.error("No subreddit control provided in the arguments")
--- topics.py
+++ topics.py
@@ -6,6 +6,7 @@ import gensim
 import pyLDAvis
 import pyLDAvis.gensim_models
 import gensim.corpora as corpora
+from gensim import models
 from gensim.models.coherencemodel import CoherenceModel
 
 
@@ -22,33 +23,48 @@ def compute_lda():
         data = [doc.split(" ") for doc in data]
 
         data_dict = corpora.Dictionary(data)
-        doc_term_matrix = [data_dict.doc2bow(doc) for doc in data]
+        data_dict.filter_extremes(no_below=15, no_above=0.75, keep_n=1000)
+        # Bag of words
+        corpus_bow = [data_dict.doc2bow(doc) for doc in data]
+        # TF-IDF
+        tfidf = models.TfidfModel(corpus_bow)
+        corpus_tfidf = tfidf[corpus_bow]
+        # Bigrams
+        bigram = gensim.models.Phrases(data, min_count=15, threshold=10)
+        bigram_mod = gensim.models.phrases.Phraser(bigram)
 
-        start_time = time.time()
-        # Build LDA model
-        lda_model = gensim.models.ldamodel.LdaModel(corpus=doc_term_matrix,
-                                                id2word=data_dict,
-                                                num_topics=10,
-                                                random_state=100,
-                                                update_every=1,
-                                                chunksize=5000,
-                                                passes=5,
-                                                alpha="auto",
-                                                per_word_topics=True)
-        print(f"Elapsed time building the model: {time.time() - start_time} seconds")
-        lda_model.print_topics()
+        word_models = [corpus_bow, corpus_tfidf, bigram_mod]
+        word_models_names = ["bow", "tfidf", "bigram"]
 
-        # Compute Perplexity
-        print(f"Perplexity: {lda_model.log_perplexity(doc_term_matrix)}")
+        for i in range(len(word_models)):
+            model_name = word_models_names[i]
+            logger.debug(f"Computing LDA model for '{model_name}'")
+            start_time = time.time()
+            # Build LDA model
+            lda_model = gensim.models.ldamodel.LdaModel(corpus=word_models[i],
+                                                    id2word=data_dict,
+                                                    num_topics=10,
+                                                    random_state=100,
+                                                    update_every=1,
+                                                    chunksize=5000,
+                                                    passes=5,
+                                                    alpha="auto",
+                                                    per_word_topics=True)
+            logger.debug(f"Elapsed time building the model ({model_name}): {time.time() - start_time} seconds")
+            lda_model.print_topics()
 
-        # Compute Coherence Score
-        coherence_model_lda = CoherenceModel(model=lda_model, corpus=doc_term_matrix, dictionary=data_dict, coherence="u_mass")
-        coherence_lda = coherence_model_lda.get_coherence()
-        print(f"Coherence Score: {coherence_lda}\n")
+            # Compute Perplexity
+            logger.debug(f"Perplexity ({model_name}): {lda_model.log_perplexity(word_models[i])}")
 
-        vis = pyLDAvis.gensim_models.prepare(lda_model, doc_term_matrix, data_dict)
-        filename = file.split('.')[1].replace("/", "_").replace("\\", "_")
-        pyLDAvis.save_html(vis, f"./output/lda{filename}.html")
+            # Compute Coherence Score
+            coherence_model_lda = CoherenceModel(model=lda_model, corpus=word_models[i], dictionary=data_dict, coherence="u_mass")
+            coherence_lda = coherence_model_lda.get_coherence()
+            logger.debug(f"Coherence Score ({model_name}): {coherence_lda}")
+
+            vis = pyLDAvis.gensim_models.prepare(lda_model, word_models[i], data_dict)
+            filename = file.split('.')[1].replace("/", "_").replace("\\", "_")
+            pyLDAvis.save_html(vis, f"./output/lda{filename}_{model_name}.html")
+            logger.debug(f"HTML out generated ({model_name})\n")
 
 
 def extract_topics():
